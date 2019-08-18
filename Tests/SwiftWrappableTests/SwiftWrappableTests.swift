549bca025f47c7e641798fcb5d9590192f2f4288
hectr <h@mrhector.me>
2019-08-18 20:13:57 +0200

    Prepare first SwiftWrappable tests

---
+++ Tests/SwiftWrappableTests/SwiftWrappableTests.swift
@@ -0,0 +1,27 @@
+import XCTest
+@testable import SwiftTypes
+
+final class SwiftWrappableTests: XCTestCase {
+    func testIsOptionalSyntacticSugarType() {
+        // TODO: @hectr
+    }
+    
+    public func testIsDictionarySyntacticSugarType() {
+        // TODO: @hectr
+    }
+    
+    public func testIsArraySyntacticSugarType() {
+        // TODO: @hectr
+    }
+    
+    public func testNormalizeReturnType() {
+        // TODO: @hectr
+    }
+    
+    static var allTests = [
+        ("testIsOptionalSyntacticSugarType", testIsOptionalSyntacticSugarType),
+        ("testIsDictionarySyntacticSugarType", testIsDictionarySyntacticSugarType),
+        ("testIsArraySyntacticSugarType", testIsArraySyntacticSugarType),
+        ("testNormalizeReturnType", testNormalizeReturnType),
+    ]
+}
