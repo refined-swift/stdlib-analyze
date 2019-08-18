e264917dd41ef63b8e969b821d4b9b0333dd0047
hectr <h@mrhector.me>
2019-08-18 22:45:08 +0200

    Implement missing tests

--- Tests/SwiftWrappableTests/SwiftWrappableTests.swift
+++ Tests/SwiftWrappableTests/SwiftWrappableTests.swift
@@ -1,21 +1,34 @@
 import XCTest
-@testable import SwiftTypes
+@testable import SwiftWrappable
 
 final class SwiftWrappableTests: XCTestCase {
     func testIsOptionalSyntacticSugarType() {
-        // TODO: @hectr
+        XCTAssertFalse(isOptionalSyntacticSugarType("Optional<Array<Int>?>"))
+        XCTAssertFalse(isOptionalSyntacticSugarType("Array<Int?>"))
+        XCTAssertTrue(isOptionalSyntacticSugarType("Array<Int>?"))
     }
     
     public func testIsDictionarySyntacticSugarType() {
-        // TODO: @hectr
+        XCTAssertFalse(isDictionarySyntacticSugarType("Dictionary<String, Int>"))
+        XCTAssertFalse(isDictionarySyntacticSugarType("Array<Dictionary<String, Int>>"))
+        XCTAssertTrue(isDictionarySyntacticSugarType("[String: Int]"))
+        XCTAssertTrue(isDictionarySyntacticSugarType("[String : Int]"))
     }
     
     public func testIsArraySyntacticSugarType() {
-        // TODO: @hectr
+        XCTAssertFalse(isArraySyntacticSugarType("Array<Int>"))
+        XCTAssertFalse(isArraySyntacticSugarType("Array<[Int]>"))
+        XCTAssertFalse(isArraySyntacticSugarType("[Int]?"))
+        XCTAssertTrue(isArraySyntacticSugarType("[String]"))
     }
     
     public func testNormalizeReturnType() {
-        // TODO: @hectr
+        XCTAssertEqual(normalizeReturnType("[String:Int]?", associatedTypes: []), "Optional<[String:Int]>") // TODO: it should normalize recursively
+        XCTAssertEqual(normalizeReturnType("[Int]", associatedTypes: []), "Array<Int>")
+        XCTAssertEqual(normalizeReturnType("[Int:Int]", associatedTypes: []), "Dictionary<Int, Int>")
+        XCTAssertEqual(normalizeReturnType("Self", associatedTypes: []), "Self")
+        XCTAssertEqual(normalizeReturnType("X?", associatedTypes: ["X"]), "Optional<WrappedValue.X>")
+        XCTAssertEqual(normalizeReturnType("Array<[X]>", associatedTypes: ["X"]), "Array<Array, WrappedValue.X>")
     }
     
     static var allTests = [
