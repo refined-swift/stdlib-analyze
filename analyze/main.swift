10defb8243b16c880ab6d2a93a0813583d5c591f
hectr <h@mrhector.me>
2019-08-25 03:17:20 +0200

    Generate Operators.json file

--- analyze/main.swift
+++ analyze/main.swift
@@ -26,7 +26,6 @@ let jsonEncoder = JSONEncoder()
 
 let features = SwiftFeature.parse(sourceryTypes: sourceryTypes,
                                   minimumCardinality: 1,
-                                  includeMethods: false,
                                   includeSubscripts: false)
 
 //////////////////////////////////////////////////////////////////////////////////
@@ -35,6 +34,7 @@ let maybeOutput = URL(fileURLWithPath: CommandLine.arguments[1])
     .appendingPathComponent("Maybe.json")
 
 let maybeFeatures = features
+    .filter { $0.featureType == .property }
     .filter { $0.featureName.hasPrefix("Maybe") }
     .filter { $0.matchingProtocols.count == 0 }
 
@@ -47,6 +47,7 @@ var propertiesOutput = URL(fileURLWithPath: CommandLine.arguments[1])
     .appendingPathComponent("Properties.json")
 
 let propertiesFeatures = features
+    .filter { $0.featureType == .property }
     .filter { !$0.featureName.hasPrefix("Maybe") }
     .filter { $0.matchingProtocols.count == 0 }
     .filter { $0.returnType != "Self" }
@@ -59,6 +60,18 @@ let propertiesFeatures = features
 
 let propertiesData = try jsonEncoder.encode(propertiesFeatures)
 try propertiesData.write(to: propertiesOutput)
+
+////////////////////////////////////////////////////////////////////////////////
+
+var operatorsOutput = URL(fileURLWithPath: CommandLine.arguments[1])
+    .appendingPathComponent("Operators.json")
+
+let operatorsFeatures = features
+    .filter { $0.featureType == .operator }
+    .filter { $0.matchingProtocols.count == 0 }
+
+let operatorsData = try jsonEncoder.encode(operatorsFeatures)
+try operatorsData.write(to: operatorsOutput)
 
 ////////////////////////////////////////////////////////////////////////////////
 
